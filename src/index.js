52111ba22a082c805a0951604fbf3e00b76a38de
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2021-04-22 11:39:59 +0530

    use Ref  examples

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const App =()=>{
+  const [myNum,setMyNum] = useState(0)
+
+  const inputOne =useRef()
+  const inputTwo =useRef()
+
+  const getNumBox=()=>{
+    console.log("HELLO")
+    console.log(inputOne.current.style)
+    inputOne.current.style.width="400px"
+    inputOne.current.style.backgroundColor="red"
+    inputOne.current.style.padding = "50px"
+  }
+
+  const getTextBox=()=>{
+    console.log("World")
+    console.log(inputTwo.current)
+  }
+
+  return(
+    <>
+    <h1>Welcome to useRef</h1>
+    <h2>
+      CDSTech
+    </h2>
+    <input 
+    ref={inputOne}
+     value={myNum}
+    type="number"
+    style={{width:'100px'}}
+    onChange={(e)=>{setMyNum(e.target.value)}}
+    />
+
+<input 
+ref={inputTwo}
+     value={myNum}
+    type="number"
+    onChange={(e)=>{setMyNum(e.target.value)}}
+    />
+
+    <h3>Value is : {myNum}</h3>
+
+    <button onClick={()=>getNumBox()}>Rupees</button>
+    <button onClick={()=>getTextBox()}>Dollers</button>
+    </>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
